5096d96c461f5b6ac15cd4612aa21b3e4cbae6b8
tmrkev <tmrkev007@gmail.com>
2026-04-12 19:20:36 +0200

    kek

--- anzred/src/components/Question_item.jsx
+++ anzred/src/components/Question_item.jsx
@@ -1,13 +1,16 @@
 function QuestionItem({ QuestionData }) {
     return (
         <div className="Question_item">
-            <div className="Question_item_text_content">
-                <div className="number">{QuestionData.number}</div>
-                <div className="question">{QuestionData.question}</div>                
+            <div className="Question_item_position">
+                <div className="Question_item_text_content">
+                    <div className="number">{QuestionData.number}</div>
+                    <div className="question">{QuestionData.question}</div>                
+                </div>
+            <img src={QuestionData.image} alt="{QuestionData.image}" />                
             </div>
-            <img src={QuestionData.image} alt="{QuestionData.image}" />
+            <hr className="line_up" />            
         </div>
-        <hr className="line_up" />
+
     )
 }
 
