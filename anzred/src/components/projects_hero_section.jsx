2a1ca1f04ec508770cbfd1950058c466f9287963
Katanoisi23 <katanoisi23@gmail.com>
2026-04-29 11:29:11 -0700

    123

--- anzred/src/components/projects_hero_section.jsx
+++ anzred/src/components/projects_hero_section.jsx
@@ -32,26 +32,26 @@ function ProjectSecButtonsRow() {
 
 function CasesSection() {
     const casesData = [
-        {id:1, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:2, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:3, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:4, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:5, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:6, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:7, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:8, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:9, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:10, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:11, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:12, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:13, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:14, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
-        {id:15, image: temporaryImage, title:'Реклама пельменей Сааеф', description:'Бизнес'},
+        { id: 1, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 2, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 3, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 4, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 5, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 6, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 7, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 8, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 9, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 10, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 11, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 12, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 13, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 14, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
+        { id: 15, image: temporaryImage, title: 'Реклама пельменей Сааеф', description: 'Бизнес' },
     ];
 
     return (
         <div className="cases_list">
-            {casesData.map ((caseData) => (
+            {casesData.map((caseData) => (
                 <CaseItem key={caseData.id} caseData={caseData} />
             ))}
         </div>
@@ -68,12 +68,12 @@ function ProjectsHeroSection() {
                     <h2 style={{ marginTop: '48px' }} className="top_text_section4">Проекты</h2>
                     <div className="projects_sections">
                         <ProjectSecButtonsRow />
-                    <div className="cases_setion">
-                        <CasesSection />
-                        <div className="button_all_projects" style={{ marginBottom: '150px' }}>
-                            <a href="#" className="all_projects_text">Все проекты</a>
+                        <div className="cases_setion">
+                            <CasesSection />
+                            <div className="button_all_projects" style={{ marginBottom: '150px' }}>
+                                <a href="#" className="all_projects_text">Все проекты</a>
+                            </div>
                         </div>
-                    </div>                        
                     </div>
                 </div>
             </div>
