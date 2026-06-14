b92c7de36da1c4fc1064a62badd6a5733420e0e2
tmrkev <tmrkev007@gmail.com>
2026-06-14 21:06:43 +0200

    jm
    kh

--- anzred/src/components/Footer.jsx
+++ anzred/src/components/Footer.jsx
@@ -5,26 +5,30 @@ function Footer () {
             <div className="container">
                 <div className="content">
                     <div className="footer_content">
+                        <div className="LefttSideForMobile">
                         <div className="logo_side">
                             <img src="./icons/logo.svg" alt="" />
                         </div>
-                        <div className="contacts">
+                        <div className="contacts"> 
                             <h3 className="title_inner_footer">Контакты</h3>
                             <p className="rest_of_text_footer">Anzored@gmail.com </p>
                             <p className="rest_of_text_footer"> +7 (938) 000-08-90</p>
+                        </div>                              
                         </div>
+                        <div className="RightSideForMobile">
                         <div className="where_find">
                             <h3 className="title_inner_footer">Офис</h3>
                             <p className="rest_of_text_footer">ул. Назарбаева 11 </p>
                             <p className="rest_of_text_footer">г. Грозный</p>
                         </div>
                         <div className="social_media">
-                            <h3 className="title_inner_footer">Подпишись</h3>
+                            <h3 className="title_inner_footer_sub">Подпишись</h3>
                             <div className="social_media_inner">
                                 <img className="socials" src="./icons/instagram.svg" alt="" />
                                 <img className="socials" src="./icons/telegram.svg" alt="" />
                                 <img className="socials" src="./icons/linkedin.svg" alt="" />                                
                             </div>
+                        </div>                             
                         </div>
                     </div>
                 </div>
--- anzred/src/components/Section__2.jsx
+++ anzred/src/components/Section__2.jsx
@@ -7,15 +7,15 @@ function Section2 () {
                         <div className="text_left">
                             <h2 className="text_left_inner">
                                 Студия «Anzored» <br />
-                                Уникальная площадка в медиасфере <br />
+                                Уникальная площадка в медиасфере <br className="brdesktop" />
                                 Чеченской Республики, которая <br />
-                                распологает большим спектром <br />
+                                распологает большим спектром <br className="brdesktop" />
                                 услуг и возможностей. <br />
                             </h2>
                         </div>
                         <div className="text_right">
                             <h2 className="text_right_inner">
-                                От рекламных роликов до полноценного фильма
+                                От рекламных роликов до <br className="brMobile"/> полноценного фильма
                             </h2>
                             <h2 className="text_right_red">15 лет в сфере медиа</h2>
                             <h2 className="text_right_inner">
